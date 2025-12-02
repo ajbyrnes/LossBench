5caf9568c12dc740ed692aa71868715717401613
ajbyrnes <ajbyrne2@uic.edu>
2025-12-02 14:03:17 -0600

    added SZ3 to compressor factory

--- src/compressors/factory.cpp
+++ src/compressors/factory.cpp
@@ -4,6 +4,7 @@
 #include <unordered_map>
 
 #include "ZlibCompressor.hpp"
+#include "SZ3Compressor.hpp"
 #include "factory.hpp"
 
 using CompressorFactory = std::unique_ptr<Compressor>(*)();
@@ -11,6 +12,7 @@ using CompressorFactory = std::unique_ptr<Compressor>(*)();
 std::unique_ptr<Compressor> createCompressor(const std::string& name) {
     static const std::unordered_map<std::string, CompressorFactory> kFactories = {
         {"zlib", +[]() -> std::unique_ptr<Compressor> { return std::make_unique<ZlibCompressor>(); }},
+        {"sz3", +[]() -> std::unique_ptr<Compressor> { return std::make_unique<SZ3Compressor>(); }},
     };
 
     const auto it = kFactories.find(name);
